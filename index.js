84e07e99e197ef56fd87ca4f7da729dfa2e528a8
ememuo <angelememuo@gmail.com>
2024-07-02 13:35:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,32 @@ const app = express();
 const axios = require("axios")
 const bodyparser = require('body-parser')
 
+app.set('trust proxy', true)
 app.use(express.json()); app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env. PORT || 8000;
-app.listen(PORT, ()=> {
-    console.log(`App is running on port ${PORT}` )
-});
 
 
 app.get('/api/hello', async(req, res) => {
-    const visitorName = req.query.visitor_name || 'Mark';
-    const client_ip = req.ip;
-
-    const api_url = `https://ipapi.co/${client_ip}/json/`;
-
-    fetch(api_url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    const temperature = 11
+    const visitorName = req.query.visitor_name || 'Visitor';
+    let client_ip = req.headers['x-forwarded-for'] || req.ip;
+    if (client_ip === '::1' || client_ip === '127.0.0.1') {
+        client_ip = '8.8.8.8';
+    }
     
+    const response = await fetch (`https://ipapi.co/${client_ip}/json/`);
+    const data = await response.json();
+    const {city, region_name, country_name, latitude, longitude } = data;
+    const temperature = 11
     res.json({
         clientIP : client_ip,
         location: `${city}`,
         greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrres Celsius in ${city}.`
     })
 })
+
+
+const PORT = process.env. PORT || 8000;
+app.listen(PORT, ()=> {
+    console.log(`App is running on port ${PORT}` )
+});
 
 module.exports = app;
