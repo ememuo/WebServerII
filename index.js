0dcbe0e39c340ff6ce085e4e3f699bcb2d9bd923
ememuo <angelememuo@gmail.com>
2024-07-02 12:58:49 +0100

    fixed ip

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const dotenv = require("dotenv")
-dotenv.config();
 const app = express();
 const bodyparser = require('body-parser')
 
 app.use(express.json()); app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
 const PORT = process.env. PORT || 8000;
 app.listen(PORT, ()=> {
     console.log(`App is running on port ${PORT}` )
@@ -18,13 +12,17 @@ app.listen(PORT, ()=> {
 
 app.get('/api/hello', async(req, res) => {
     const visitorName = req.query.visitor_name || 'Mark';
-    const client_ip = req.ip
-    const location = 'New York'
-    const temp = 11;
+    const client_ip = req.ip;
+
+    const response = await fetch (`https://ipapi.co/${client_ip}/json/`);
+    const data = await response.json();
+    const {city, region_name, country_name, latitude, longitude } = data;
+    const temperature = 11
+    
     res.json({
         clientIP : client_ip,
-        location: location,
-        greeting: `Hello, ${visitorName}! The temperature is ${temp} degrres Celsius in ${location}.`
+        location: `${city}`,
+        greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrres Celsius in ${city}.`
     })
 })
 
