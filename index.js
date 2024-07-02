cca44bf12282e41503b6e6377aa8262bff42c38d
ememuo <angelememuo@gmail.com>
2024-07-02 16:55:49 +0100

    Changed Spelling

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json()); app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/hello', async(req, res) => {
     const visitorName = req.query.visitor_name || 'Visitor';
-    let client_ip = req.headers['x-forwarded-for'] || req.socket.address;
-    if (client_ip === '::1' || client_ip === '127.0.0.1') {
-        client_ip = '8.8.8.8';
+    let clientIP = req.headers['x-forwarded-for'] || req.ip;
+    if (clientIP === '::1' || clientIP === '127.0.0.1') {
+        clientIP = '8.8.8.8';
     }
     
     const response = await fetch (`https://ipapi.co/${client_ip}/json/`);
@@ -19,9 +19,9 @@ app.get('/api/hello', async(req, res) => {
     const {city, region_name, country_name, latitude, longitude } = data;
     const temperature = 11
     res.json({
-        clientIP : client_ip,
+        client_ip : clientIP,
         location: `${city}`,
-        greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${city}.`
+        greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrres Celsius in ${city}.`
     })
 })
 
